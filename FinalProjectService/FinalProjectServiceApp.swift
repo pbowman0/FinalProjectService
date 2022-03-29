d0a8ad6b47109c0e6246941c4c9813e9ea8bf167
pbowman0 <paigeelisabethbowman@gmail.com>
2022-03-29 10:22:04 -0500

    Initial Commit

---
+++ FinalProjectService/FinalProjectServiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectServiceApp.swift
+//  FinalProjectService
+//
+//  Created by Student on 3/29/22.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectServiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
